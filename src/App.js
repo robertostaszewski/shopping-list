5a011e3431c53069d7c37283a613bbad0651e2d2
Robert_Ostaszewski <Robert.Ostaszewski@refinitiv.com>
2021-05-25 15:00:26 +0200

    logging in functionality added

--- src/App.js
+++ src/App.js
@@ -1,99 +1,143 @@
 import React from 'react';
-import { Client, } from '@stomp/stompjs';
+import SockJS from 'sockjs-client';
+import Stomp from 'stompjs';
+import { useHistory } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from "react-router-dom";
+
 
 const URL = 'http://localhost:8080/rest'
 
-const client = new Client({
-  brokerURL: 'ws://localhost:8080/gs-guide-websocket',
-  debug: function (str) {
-    console.log(str);
-  }
-})
 
 const App = () => {
-  const [featchedLists, setLists] = React.useState([]);
-  const [products, setProducts] = React.useState([]);
 
-  React.useEffect(() => {
-    const fetchJson = url =>
-      fetch(url).then(response => response.json())
-
-    const updateProducts = listId =>
-      fetchJson(`${URL}/lists/${listId}/products`)
-        .then(result => setProducts(p => [...result, ...p.filter(product => product.listId !== listId)]))
-
-    fetchJson(`${URL}/lists`)
-      .then(lists => {
-        setLists(lists)
-        lists.forEach(list => updateProducts(list.id))
-
-        client.onConnect = frame => {
-          lists.forEach(list => {
-            client.subscribe(`/topic/${list.id}`, msg => updateProducts(list.id))
-          })
-
-          client.subscribe("/topic/me", msg =>
-            fetchJson(`${URL}/lists`)
-              .then(lists => {
-                setLists(lists)
-                console.log(msg.body)
-                updateProducts(msg.body)
-                client.subscribe(`/topic/${msg.body}`, msgq => updateProducts(msg.body))
-              }))
-        }
-
-        client.activate();
-      })
-  }, []);
-
-  const addProductMethod = (listId, productName) => {
-    const requestOptions = {
-      method: 'PUT'
-    };
-
-    fetch(`${URL}/lists/${listId}?productName=${productName}`, requestOptions)
-  }
-
-  const deleteMethod = (listId, productId) => {
-    const requestOptions = {
-      method: 'DELETE'
-    };
-
-    fetch(`${URL}/lists/${listId}/${productId}`, requestOptions)
-  }
-
-  const addListMethod = (listName) => {
-    const requestOptions = {
-      method: 'PUT'
-    };
-
-    fetch(`${URL}/lists?listName=${listName}`, requestOptions)
-  }
-
-  const deleteListMethod = (listId) => {
-    const requestOptions = {
-      method: 'DELETE'
-    };
-
-    fetch(`${URL}/lists/${listId}`, requestOptions)
-    
-    client.unsubscribe(`/topic/${listId}`)
+  const [authHeader, setAuth] = React.useState(new Headers({
+  }));
+  const [userName, setText] = React.useState("");
+  const [pass, setPass] = React.useState("");
+ 
+  const onLogin = (username, pass) => {
+    setAuth(new Headers({
+      'Authorization': 'Basic ' + Buffer.from(`${username}:${pass}`).toString('base64')
+    }));
+    setText(username);
+    setPass(pass);
   }
 
   return (
-    < div >
-      <h1>Shopping app</h1>
-      <hr />
-      <Adder label="Add List: " addtMethod={addListMethod} />
-      <List lists={featchedLists} allProducts={products} onAddProduct={addProductMethod} onDelete={deleteMethod} listDelete={deleteListMethod} />
-    </div >
+    <Router>
+
+        <Switch>
+          <Route path="/app">
+          <MainApp authHeader={authHeader} user={userName} pass={pass}/>
+          </Route>
+          <Route path="/">
+            <Login onLogin={onLogin}/>
+          </Route>
+        </Switch>
+    </Router>
   );
 };
 
+const MainApp = ({ authHeader, user, pass }) => {
+  const socket = new SockJS('http://localhost:8080/gs-guide-websocket');
+  const client = Stomp.over(socket)
+  const [featchedLists, setLists] = React.useState([]);
+  const [products, setProducts] = React.useState([]);
+
+  const fetchh = (url, method) =>
+    fetch(url, { method: method, headers: authHeader })
+
+  React.useEffect(() => {
+    const fetchJson = (url, method) =>
+      fetch(url, { method: method, headers: authHeader}).then(response => response.json())
+
+    const updateProducts = listId =>
+      fetchJson(`${URL}/lists/${listId}/products`, 'GET')
+        .then(result => setProducts(p => [...result, ...p.filter(product => product.listId !== listId)]))
+
+      fetchJson(`${URL}/lists`, 'GET')
+      .then(lists => {
+        setLists(lists)
+        lists.forEach(list => updateProducts(list.id))
+        client.connect(`${user}`, `${pass}`, frame => {
+
+          lists.forEach(list => {
+            client.subscribe(`/topic/${list.id}`, msg => updateProducts(list.id))
+          })
+          client.subscribe("/topic/me", msg =>
+          fetchJson(`${URL}/lists`, 'GET')
+            .then(lists => {
+              setLists(lists)
+              lists.forEach(list => {
+                updateProducts(list.id)
+              client.subscribe(`/topic/${msg.body}`, msgq => updateProducts(msg.body))
+              })
+            }))
+        })
+      })
+  
+  }, [authHeader, user, pass]);
+
+  const addProductMethod = (listId, productName) => {
+    fetchh(`${URL}/lists/${listId}?productName=${productName}`, 'PUT')
+  }
+
+  const deleteMethod = (listId, productId) => {
+
+    fetchh(`${URL}/lists/${listId}/${productId}`, 'DELETE')
+  }
+
+  const addListMethod = (listName) => {
+
+    fetchh(`${URL}/lists?listName=${listName}`, 'PUT')
+  }
+
+  const deleteListMethod = (listId) => {
+
+    fetchh(`${URL}/lists/${listId}`, 'DELETE')
+
+    client.unsubscribe(`/topic/${listId}`)
+  }
+  
+  return(
+    < div >
+    <h1>Shopping app</h1>
+    <hr />
+    <Adder label="Add List: " addtMethod={addListMethod} />
+    <List lists={featchedLists} allProducts={products} onAddProduct={addProductMethod} onDelete={deleteMethod} listDelete={deleteListMethod} />
+  </div >
+  )
+}
+
+const Login = ({ onLogin }) => {
+  const history = useHistory();
+  const [userName, setText] = React.useState();
+  const [pass, setPass] = React.useState();
+
+  return (
+    <>
+      <label>Login</label>
+      <input type="text" onChange={event => setText(event.target.value)} />
+      <label>Pass</label>
+      <input type="text" onChange={event => setPass(event.target.value)} />
+      <button onClick={() =>  {
+        onLogin(userName, pass) 
+        history.push("/app")
+      }}>
+          Submit
+        </button>
+    </>
+  )
+}
+
 const List = ({ lists, allProducts, onAddProduct, onDelete, listDelete }) =>
-  <table style={{ border: "1px solid black", borderCollapse: "collapse"}}>
+  <table style={{ border: "1px solid black", borderCollapse: "collapse" }}>
     <thead>
-      <tr style={{ border: "1px solid black", borderCollapse: "collapse"}}>
+      <tr style={{ border: "1px solid black", borderCollapse: "collapse" }}>
         <th >Name</th>
         <th >Number of elements in the list</th>
         <th ></th>
@@ -116,7 +160,7 @@ const ListEntry = ({ list, products, onAddProduct, onDelete, listDelete }) => {
 
   return (
     <>
-      <tr style={{ border: "1px solid black"}}>
+      <tr style={{ border: "1px solid black" }}>
         <td >{list.listName}</td>
         <td >{products.length}</td>
         <td >
@@ -155,7 +199,7 @@ const ProductList = ({ listId, products, onAddProduct, onDelete }) => {
 const ProductEntry = ({ product, deleteProduct }) =>
   <li>
     <span>{product.name}</span>
-    <button style={{float: "right"}} onClick={() => deleteProduct(product.productId)} >Delete</button>
+    <button style={{ float: "right" }} onClick={() => deleteProduct(product.productId)} >Delete</button>
   </li>
 
 
